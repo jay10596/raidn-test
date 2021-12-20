a2f2c0da3986583008b3b5b5309c9958761cb4db
Jay Modi <jay.modi105@gmail.com>
2021-12-20 21:41:00 +1000

    Setup database, migration adn factories

---
+++ app/Models/Card.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Card extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'number',
+        'cvv',
+        'type',
+        'owner',
+        'expiration_date',
+        'is_valid'
+    ];
+}
---
+++ database/factories/CardFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CardFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()    
+    {
+        $number = $this->faker->randomNumber(8, true) . $this->faker->randomNumber(8, true);
+        $cvv = $this->faker->randomNumber(3, true);
+
+        return [
+            'number' => Helper::set_number_format($number),
+            'cvv' => $cvv,
+            'type' => Helper::check_card_type($number[0]),
+            'owner' => $this->faker->name,
+            'expiration_date' => $this->faker->dateTimeBetween('+1 week', '+3 years'),
+            'is_valid' => Helper::is_valid(str_replace(' ', '', $number))
+        ];
+    }
+}
