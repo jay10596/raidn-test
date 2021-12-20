cd49ed2007aed7f36eec37649895b75b5f42cb1e
Jay Modi <jay.modi105@gmail.com>
2021-12-20 21:43:47 +1000

    Make web and api routes to check on Postman

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CardController;
+use App\Http\Controllers\TempController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('cards/{filter}', [CardController::class, 'filter']);
+
+Route::resource('cards', CardController::class);
